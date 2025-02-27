58091e96e8cc59ab4c706730646bbb3f8ed6b051
Jaimin Prajapati <jaimin.prajapati-i@equestsolutions.net>
2025-02-27 17:35:42 +0530

    Updating code

--- frontend/src/Data/ApiURLS.js
+++ frontend/src/Data/ApiURLS.js
@@ -1,47 +1,15 @@
 const ApiURLS = {
   // User
-
-  // Registration
-  Register: "/api/user/register",
-  VerifyUser: "/api/user/verify-user",
-  UpdateUser: "/api/user/update-user",
-
-  // Login
   Login: "/api/user/login",
+  Register: "/api/user/register",
+  UpdateUser: "/api/user/update-user",
   Logout: "/api/user/logout",
-  SendingMailForLoginUser: "/api/user/sending-mail-for-login",
-  RefreshAccessToken: "/api/user/refresh-tokens",
-
-  // Admin Requests
-  GetSingleUser: (id) => `/api/user/single-user/${id}`,
-  DeleteUser: (id) => `/api/user/delete/${id}`,
-  GetAllOwnOrder: "/api/user/get-all-own-orders",
-
-  // Customization Options
-  AddCustomizationOptions:
-    "/api/customization-options/add-customization-options",
-  DeleteCustomizationOptions:
-    "/api/customization-options/delete/delete-customization-options",
-  GetCustomizationOptions:
-    "/api/customization-options/get-customization-options",
-
-  // Order
-  InitiateOrder: "/api/order/initiate-order",
-  AddToCartOrder: "/api/order/add-to-cart-order",
-  GetAllCartOrder: "/api/order/get-cart-order",
-  CartToOrder: "/api/order/cart-to-order",
-  AddOrder: "/api/order/add-order",
-  UpdateOrderStatus: (id) => `/api/order/update-state/${id}`,
-  GetSingleOrder: (id) => `/api/order/single-order/${id}`,
-  GetAllOrder: "/api/order/get-all-orders",
 
   // Product
-  AddProduct: "/api/product/add-product",
+  AddProduct: "/api/product/update-user",
+  DeleteProduct: "/api/product/delete",
+  GetAllProduct: "api/product/get-all-products",
   UpdateProduct: "/api/product/update-product",
-  DeleteProduct: (id) => `/api/product/delete/${id}`,
-  GetSingleProduct: (id) => `/api/product/single-product/${id}`,
-  GetAllProduct: "/api/product/get-all-products",
-  GetAllCustomer: "/api/product/get-all-customers",
 };
 
 export default ApiURLS;
